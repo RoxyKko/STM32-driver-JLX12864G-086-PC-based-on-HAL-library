44101121a990d299156ac43c31a2c80dd28c6e92
RoxyKko <86786320+RoxyKko@users.noreply.github.com>
2022-05-04 22:31:19 +0800

    Add files via upload

---
+++ Debug/Drivers/LCD12864/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/LCD12864/lcd12864.c 
+
+OBJS += \
+./Drivers/LCD12864/lcd12864.o 
+
+C_DEPS += \
+./Drivers/LCD12864/lcd12864.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/LCD12864/%.o Drivers/LCD12864/%.su: ../Drivers/LCD12864/%.c Drivers/LCD12864/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"D:/cubeIDE/oled/Drivers/LCD12864" -I"D:/cubeIDE/oled/Drivers/base" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-LCD12864
+
+clean-Drivers-2f-LCD12864:
+	-$(RM) ./Drivers/LCD12864/lcd12864.d ./Drivers/LCD12864/lcd12864.o ./Drivers/LCD12864/lcd12864.su
+
+.PHONY: clean-Drivers-2f-LCD12864
+
---
+++ Debug/Drivers/base/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/base/base.c 
+
+OBJS += \
+./Drivers/base/base.o 
+
+C_DEPS += \
+./Drivers/base/base.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/base/%.o Drivers/base/%.su: ../Drivers/base/%.c Drivers/base/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"D:/cubeIDE/oled/Drivers/LCD12864" -I"D:/cubeIDE/oled/Drivers/base" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-base
+
+clean-Drivers-2f-base:
+	-$(RM) ./Drivers/base/base.d ./Drivers/base/base.o ./Drivers/base/base.su
+
+.PHONY: clean-Drivers-2f-base
+
